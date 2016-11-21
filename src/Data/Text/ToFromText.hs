322d2784c2552b8b86da84d3cfc8f606357484f8
Alexander Thiemann <mail@athiemann.net>
2016-11-21 03:35:45 +0100

    switch to monad in ToFromText to allow better error messages

--- src/Data/Text/ToFromText.hs
+++ src/Data/Text/ToFromText.hs
@@ -3,14 +3,13 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Data.Text.ToFromText where
 
-import Control.Applicative
 import qualified Data.Text as T
 
 class ToFromText a where
     toText :: a -> T.Text
-    fromText :: Alternative m => T.Text -> m a
+    fromText :: Monad m => T.Text -> m a
 
-    default fromText :: (Enum a, Bounded a, Alternative m) => T.Text -> m a
+    default fromText :: (Enum a, Bounded a, Monad m) => T.Text -> m a
     fromText = fromTextHelper toText
 
 instance ToFromText T.Text where
@@ -21,10 +20,10 @@ instance ToFromText String where
     toText = T.pack
     fromText = pure . T.unpack
 
-fromTextHelper :: (Enum a, Bounded a, Alternative m) => (a -> T.Text) -> T.Text -> m a
+fromTextHelper :: (Enum a, Bounded a, Monad m) => (a -> T.Text) -> T.Text -> m a
 fromTextHelper mkT q =
     loop [minBound .. maxBound]
     where
-      loop [] = empty
+      loop [] = fail ("Could not read " ++ show q)
       loop (x : xs) =
           if mkT x == q then pure x else loop xs

71cabda6633f3874c575d0aa47e94335f76012ea
Alexander Thiemann <mail@athiemann.net>
2020-05-17 19:30:44 -0700

    fix monad fail issues

--- src/Data/Text/ToFromText.hs
+++ src/Data/Text/ToFromText.hs
@@ -7,9 +7,9 @@ import qualified Data.Text as T
 
 class ToFromText a where
     toText :: a -> T.Text
-    fromText :: Monad m => T.Text -> m a
+    fromText :: (MonadFail m, Monad m) => T.Text -> m a
 
-    default fromText :: (Enum a, Bounded a, Monad m) => T.Text -> m a
+    default fromText :: (Enum a, Bounded a, MonadFail m, Monad m) => T.Text -> m a
     fromText = fromTextHelper toText
 
 instance ToFromText T.Text where
@@ -20,7 +20,7 @@ instance ToFromText String where
     toText = T.pack
     fromText = pure . T.unpack
 
-fromTextHelper :: (Enum a, Bounded a, Monad m) => (a -> T.Text) -> T.Text -> m a
+fromTextHelper :: (Enum a, Bounded a, MonadFail m, Monad m) => (a -> T.Text) -> T.Text -> m a
 fromTextHelper mkT q =
     loop [minBound .. maxBound]
     where
